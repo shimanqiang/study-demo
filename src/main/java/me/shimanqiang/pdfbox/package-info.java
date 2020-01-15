56cf8a4ff5e43d683c583d80c8a1483437d12efb
shimanqiang <shimanqiang@meituan.com>
2020-01-15 17:02:06 +0800

    pdfbox &  font

---
+++ src/main/java/me/shimanqiang/pdfbox/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 字体下载：
+ * http://font.chinaz.com/
+ * <p>
+ * https://www.google.com/get/noto/help/cjk/
+ *
+ * @author shimanqiang
+ * @since 2020/1/15 14:13
+ */
+package me.shimanqiang.pdfbox;
